a062d111306c662b1721ab8c5460ecda846b5ffa
manideep <17101992m@gmail.com>
2023-03-15 12:18:11 +0530

    used createSelector from reselect to memoized the categoriesSelector to re-render only when the caretories.reducer output value changed

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,6 +1,17 @@
-export const selectCategoriesMap = (state) =>
-    state.categories.categories.reduce((acc, category) => {
+import { createSelector } from "reselect";
+
+const selectCategoriesReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+    [selectCategoriesReducer], // this will be called in output function
+    (categoriesSlice) => categoriesSlice.categories
+)
+
+export const selectCategoriesMap = createSelector(
+    [selectCategories], // As long as categories array doesn't change do not run the below method
+    (categories) => categories.reduce((acc, category) => {
         const { title, items } = category;
         acc[title.toLowerCase()] = items;
         return acc
     }, {})
+)
